8226107e220275b136ec7f9a0c52265d9674eb08
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 00:01:49 +0600

    Addes Course DTOs.

---
+++ SmartSchool.Core/Models/Course/DTOs/CourseDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Core.Models.Course.DTOs
+{
+    public class CourseDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CourseCode { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public CourseType CourseType { get; set; }
+    }
+}
