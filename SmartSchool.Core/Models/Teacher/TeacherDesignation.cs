3479678231b9b297fb1f5c652106bb40af31ce19
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 00:04:03 +0600

    Added TeacherDesignation Enum.

---
+++ SmartSchool.Core/Models/Teacher/TeacherDesignation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Core.Models.Teacher
+{
+    public enum Designation
+    {
+        Professor,
+        AssociateProfessor,
+        AssistantProfessor,
+        Lecturer
+    }
+}
