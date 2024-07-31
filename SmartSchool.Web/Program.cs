f4c7cce73fca0c2e3b6107286377c7e7df6d75c9
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 01:11:37 +0600

    Registered ApplicationDbContext class to program.cs

--- SmartSchool.Web/Program.cs
+++ SmartSchool.Web/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.Repository.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSqlConnection"));
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
