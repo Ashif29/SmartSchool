37896d3b0ea5b7b506e0f1b3402782b7daac5e2e
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 00:15:40 +0600

    Added ApplicationDbContext class.

---
+++ SmartSchool.Repository/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.Core.Models.Student;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Repository.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
