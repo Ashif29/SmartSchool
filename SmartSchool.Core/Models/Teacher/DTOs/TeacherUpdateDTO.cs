b3686fa165c340d28006a80d1cabcbb111f37400
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 00:09:11 +0600

    Added Teacher DTOs.

---
+++ SmartSchool.Core/Models/Teacher/DTOs/TeacherUpdateDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Core.Models.Teacher.DTOs
+{
+    public class TeacherUpdateDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        public Designation Designation { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
