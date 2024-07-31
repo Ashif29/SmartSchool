0abd9f42fc167a70b3d28772fb08ee95d3ec7377
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-07-31 23:56:14 +0600

    Added Student DTOs.

---
+++ SmartSchool.Core/Models/Student/DTOs/StudentCreateDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Core.Models.Student.DTOs
+{
+    public class StudentCreateDTO
+    {
+        [Required]
+        [MaxLength(30)]
+
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Address { get; set; }
+
+        [Required]
+
+        public Gender Gender { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
