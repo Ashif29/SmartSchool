3bed068c29789da92487c8c9c2b01f4a00558d59
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 19:42:58 +0600

    Added Course Create Action

--- SmartSchool.Web/Controllers/CourseController.cs
+++ SmartSchool.Web/Controllers/CourseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.Core.Models.Course;
 using SmartSchool.Service.Services.Contracts;
 
 namespace SmartSchool.Web.Controllers
@@ -15,6 +17,27 @@ namespace SmartSchool.Web.Controllers
         {
             var courses = await _courseService.GetAllAsync();
             return View(courses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Course course)
+        {
+            if (course.Name == course.Description)
+            {
+                ModelState.AddModelError("Name", "Name and Description cannot be the same");
+                ModelState.AddModelError("Description", "Name and Description cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _courseService.CreateAsync(course);
+                TempData["success"] = "The villa has been created successfully.";
+            }
+            return View();
         }
     }
 }
