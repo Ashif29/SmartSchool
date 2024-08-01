7b95f7e5f18da1fd6339fee3be6417c999a2604d
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-01 17:59:29 +0600

    Created Course Controller

---
+++ SmartSchool.Web/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.Service.Services.Contracts;
+
+namespace SmartSchool.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        
+    }
+}
