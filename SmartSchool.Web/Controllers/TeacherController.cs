8384d48686e4f603eb2d25919d8d22b3a8a58dc4
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-07 13:42:39 +0600

    Added Course Controller and Index view.

---
+++ SmartSchool.Web/Controllers/TeacherController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.Service.Services.Contracts;
+using SmartSchool.Service.Services.Implementations;
+
+namespace SmartSchool.Web.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacherService _teacherService;
+
+        public TeacherController(ITeacherService teacherService)
+        {
+            _teacherService = teacherService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<JsonResult> GetTeacherList()
+        {
+            var teachers = await _teacherService.GetAllAsync();
+            return new JsonResult(teachers);
+        }
+    }
+}
