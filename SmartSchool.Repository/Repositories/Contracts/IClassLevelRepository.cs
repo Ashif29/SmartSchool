945458442dacd60eb93a5e55e0a31f99a268679c
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-10 12:45:14 +0600

    Added ClassLevelRepository

---
+++ SmartSchool.Repository/Repositories/Contracts/IClassLevelRepository.cs
@@ -0,0 +1,15 @@
+﻿using SmartSchool.Core.Models;
+using SmartSchool.Core.Models.CourseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Repository.Repositories.Contracts
+{
+    public interface IClassLevelRepository
+    {
+        Task<List<ClassLevel>> GetAllAsync();
+    }
+}
---
+++ SmartSchool.Repository/Repositories/Implementations/ClassLevelReopsitory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartSchool.Core.Models;
+using SmartSchool.Repository.DataAccess;
+using SmartSchool.Repository.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Repository.Repositories.Implementations
+{
+    public class ClassLevelReopsitory : IClassLevelRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ClassLevelReopsitory(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<List<ClassLevel>> GetAllAsync()
+        {
+            return await _db.ClassLevels.ToListAsync();
+        }
+    }
+}
