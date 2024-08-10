e08a666c3ab58b38b1890f5b134a5f9c79d1016f
Ashif Iqubal <asif.duet.cse19@gmail.com>
2024-08-10 12:45:52 +0600

    Added ClassLevelService

---
+++ SmartSchool.Service/Services/Contracts/IClassLevelService.cs
@@ -0,0 +1,15 @@
+﻿using SmartSchool.Core.Models;
+using SmartSchool.Core.Models.CourseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Service.Services.Contracts
+{
+    public interface IClassLevelService
+    {
+        Task<List<ClassLevel>> GetAllAsync();
+    }
+}
---
+++ SmartSchool.Service/Services/Implementations/ClassLevelService.cs
@@ -0,0 +1,25 @@
+﻿using SmartSchool.Core.Models;
+using SmartSchool.Repository.Repositories.Contracts;
+using SmartSchool.Service.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Service.Services.Implementations
+{
+    public class ClassLevelService : IClassLevelService
+    {
+        private readonly IClassLevelRepository _classLevelRepository;
+
+        public ClassLevelService(IClassLevelRepository classLevelRepository)
+        {
+            _classLevelRepository = classLevelRepository;
+        }
+        public async Task<List<ClassLevel>> GetAllAsync()
+        {
+            return await _classLevelRepository.GetAllAsync();
+        }
+    }
+}
